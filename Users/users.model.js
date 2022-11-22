c9ad33a004c743d9814a38b0484549ebadd52525
David TEIXEIRA <david.teixeira2@edu.devinci.fr>
2022-11-22 17:48:14 +0100

    TP4 start

---
+++ Users/users.model.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+const user = new mongoose.Schema({
+    username : String,
+    password : String
+})
+const User = mongoose.model('User',user)
+module.exports = User
